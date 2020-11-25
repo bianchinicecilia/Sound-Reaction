aaac4af9022be76c8c058223365de5e3a0649136
bianchinicecilia <bianchini.cecilia@gmail.com>
2020-11-25 22:28:41 +0000

    boh

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
 
 function loaded() {
   button = createButton('play');
-  button.mousePressed(info);
+  button.mouseOver(info);
   /*button.mousePressed(togglePlaying);*/
 }
 
@@ -24,7 +24,7 @@ function info() {
   rect(100,100,100,100);
 }
 
-/*function draw() {
+function draw() {
   background('#F4EDCD');
 
   var vol = amp.getLevel();
@@ -44,4 +44,4 @@ function togglePlaying() {
     song.stop();
     button.html('play');
   }
-}*/
+}
